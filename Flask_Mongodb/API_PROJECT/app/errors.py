aa6c566dcbb9c5c53f79ed24c4416d02be5db1a8
Joaomesq1 <1170830@isep.ipp.pt>
2020-03-23 13:01:02 +0000

    Changes on the error handlers

--- Flask_Mongodb/API_PROJECT/app/errors.py
+++ Flask_Mongodb/API_PROJECT/app/errors.py
@@ -1,12 +1,18 @@
 from flask import render_template
 from app import app
+from pymongo import errors
+
+
+@app.errorhandler(errors.OperationFailure)
+def internal_operation_error(error):
+    return render_template('error_templates/code_errors/500.html', error=error), 500
+
+
+@app.errorhandler(errors.WriteError)
+def internal_write_error(error):
+    return render_template('error_templates/code_errors/500.html', error=error), 500
 
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('error_templates/code_errors/404.html', error=error), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('error_templates/code_errors/500.html', error=error), 500
--- Flask_Mongodb/API_PROJECT/app/interface_routes.py
+++ Flask_Mongodb/API_PROJECT/app/interface_routes.py
@@ -1,72 +1,57 @@
-import jsonschema
-from pymongo import errors
 from flask import render_template, jsonify, request
 from pymongo import ReturnDocument
-
 from app import app, collection
 
 
 @app.route('/<string:switch>/interfaces.html', methods=['GET'])
 def get_interfaces_from_switch_html(switch):
-    try:
-        interfaces_list = collection.find({"switch": switch})
+    interfaces_list = collection.find({"switch": switch})
 
-        if interfaces_list.count() != 0:
-            return \
-                render_template("routes_templates/switch_interfaces.html", switch_name=switch, interfaces=interfaces_list)
-        else:
-            return render_template('error_templates/invalid_parameter.html')
-    except Exception as e:
-        return f'A problem was found during: {e.__str__()}'
+    if interfaces_list.count() != 0:
+        return \
+            render_template("routes_templates/switch_interfaces.html", switch_name=switch, interfaces=interfaces_list)
+    else:
+        return render_template('error_templates/invalid_parameter.html')
 
 
 @app.route('/<string:switch>/<string:interface>/details.html', methods=['GET'])
 def get_interface_detail_html(switch, interface):
-    try:
-        details = collection.find_one({"switch": switch, "interface": interface})
+    details = collection.find_one({"switch": switch, "interface": interface})
 
-        if details:
-            return \
-                render_template("routes_templates/interface_detail.html",
-                                switch_name=switch,
-                                interface_name=interface,
-                                interface_details=details)
-        else:
-            return render_template('error_templates/invalid_parameter.html')
-    except Exception as e:
-        return f'A problem was found during: {e.__str__()}'
+    if details:
+        return \
+            render_template("routes_templates/interface_detail.html",
+                            switch_name=switch,
+                            interface_name=interface,
+                            interface_details=details)
+    else:
+        return render_template('error_templates/invalid_parameter.html')
 
 
 @app.route('/<string:switch>/interfaces.json', methods=['GET'])
 def get_interfaces_from_switch_json(switch):
-    try:
-        interfaces_list = collection.find({"switch": switch})
+    interfaces_list = collection.find({"switch": switch})
 
-        dic = {switch: {}}
+    dic = {switch: {}}
 
-        for interface in interfaces_list:
-            dic[switch][interface['interface']] = \
-                {'Description': interface['description'], 'State': interface['state']}
+    for interface in interfaces_list:
+        dic[switch][interface['interface']] = \
+            {'Description': interface['description'], 'State': interface['state']}
 
-        return jsonify(dic)
-    except Exception as e:
-        return f'A problem was found during: {e.__str__()}'
+    return jsonify(dic)
 
 
 @app.route('/<string:switch>/<string:interface>/details.json', methods=['GET'])
 def get_interface_detail_json(switch, interface):
-    try:
-        details = collection.find_one({"switch": switch, "interface": interface})
+    details = collection.find_one({"switch": switch, "interface": interface})
 
-        dic = {details['interface']: {'Switch name': details['switch'],
-                                      'Description': details['description'],
-                                      'State': details['state']
-                                      }
-               }
+    dic = {details['interface']: {'Switch name': details['switch'],
+                                  'Description': details['description'],
+                                  'State': details['state']
+                                  }
+           }
 
-        return jsonify(dic)
-    except Exception as e:
-        return f'A problem was found during: {e.__str__()}'
+    return jsonify(dic)
 
 
 # PATCH ROUTE BY INTERFACE NAME
@@ -76,20 +61,18 @@ def patch_interface_description(switch, interface):
     dic = {"$set": payload}
     query_filter = {"switch": switch, "Interface name": interface}
     
-    try:
-        result = collection.find_one_and_update(query_filter,
+
+    result = collection.find_one_and_update(query_filter,
                                                 dic,
                                                 upsert=False,
                                                 return_document=ReturnDocument.AFTER)
     
-        res = {'Switch name': result['switch'],
+    res = {'Switch name': result['switch'],
                'Interface name': result['interface'],
                'Description': result['description'],
                'State': result['state']}
     
-        return jsonify(res)
-    except Exception as e:
-    return f'A problem was found during: {e.__str__()}'"""
+    return jsonify(res)"""
 
 
 # PATCH ROUTE BY ID
@@ -99,36 +82,30 @@ def patch_interface_description_id(switch, _id):
     dic = {"$set": payload}
     query_filter = {"switch": switch, "_id": _id}
 
-    try:
-        result = collection.find_one_and_update(query_filter,
-                                                dic,
-                                                upsert=False,
-                                                return_document=ReturnDocument.AFTER)
-        print(result)
-        res = {'Switch name': result['switch'],
-               'Interface name': result['interface'],
-               'Description': result['description'],
-               'State': result['state']}
+    result = collection.find_one_and_update(query_filter,
+                                            dic,
+                                            upsert=False,
+                                            return_document=ReturnDocument.AFTER)
 
-        return jsonify(res)
-    except Exception as e:
-        return f'A problem was found during: {e.__str__()}'
+    res = {'Switch name': result['switch'],
+           'Interface name': result['interface'],
+           'Description': result['description'],
+           'State': result['state']}
+
+    return jsonify(res)
 
 
 @app.route('/add_interface', methods=['POST'])
 def add_interface():
     payload = request.json
 
-    try:
-        post_id = collection.insert_one(payload).inserted_id
+    post_id = collection.insert_one(payload).inserted_id
 
-        result = collection.find_one({"_id": post_id})
+    result = collection.find_one({"_id": post_id})
 
-        res = {'Switch name': result['switch'],
-               'Interface name': result['interface'],
-               'Description': result['description'],
-               'State': result['state']}
+    res = {'Switch name': result['switch'],
+           'Interface name': result['interface'],
+           'Description': result['description'],
+           'State': result['state']}
 
-        return jsonify(res)
-    except errors.WriteError as e:
-        return f'A problem was found during creation of the document: {e.__str__()}', 400
+    return jsonify(res)
